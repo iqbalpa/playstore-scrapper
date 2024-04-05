5c4d87ae99c2c91e659b5898d41c4d9c5441d041
iqbalpa <iqbalpahlevi16@gmail.com>
2024-04-05 21:35:50 +0700

    update

--- reviews.py
+++ reviews.py
@@ -2,22 +2,26 @@ from google_play_scraper import Sort, reviews
 import utils
 
 list_app = ['segari', 'astro', 'sayurbox', 'titipku', 'my super indo', 'hypermart', 'happyfresh']
+list_app = {
+    'segari': 'id.segari.customer', 
+    'astro': 'com.astro.shop',
+    'sayurbox': 'com.sayurbox',
+    'hypermart': 'com.hypermart.mobile',
+    'happyfresh': 'com.happyfresh.android'
+}
 
-result, continuation_token = reviews(
-    'com.sayurbox',
-    lang='id', # defaults to 'en'
-    country='id', # defaults to 'us'
-    sort=Sort.NEWEST, # defaults to Sort.NEWEST
-    count=3, # defaults to 100
-    filter_score_with=5 # defaults to None(means all score)
-)
+for app in list_app:
+    result, continuation_token = reviews(
+        list_app[app],
+        lang='id', # defaults to 'en'
+        country='id', # defaults to 'us'
+        sort=Sort.NEWEST, # defaults to Sort.NEWEST
+        count=100, # defaults to 100
+        filter_score_with=None # defaults to None(means all score)
+    )
+    result, _ = reviews(
+        list_app[app],
+        continuation_token=continuation_token # defaults to None(load from the beginning)
+    )
 
-# If you pass `continuation_token` as an argument to the reviews function at this point,
-# it will crawl the items after 3 review items.
-
-result, _ = reviews(
-    'com.sayurbox',
-    continuation_token=continuation_token # defaults to None(load from the beginning)
-)
-
-print(result)
+    utils.save_data(result, f"data/{app}.json")
