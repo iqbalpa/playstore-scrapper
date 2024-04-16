7da857c5445bda02160a67537fecd829ac538333
iqbalpa <iqbalpahlevi16@gmail.com>
2024-04-16 18:51:49 +0700

    update utils and use utf-8

--- reviews.py
+++ reviews.py
@@ -6,7 +6,7 @@ list_app = {
     'astro': 'com.astro.shop',
     'sayurbox': 'com.sayurbox',
     'hypermart': 'com.hypermart.mobile',
-    'happyfresh': 'com.happyfresh.android'
+    # 'happyfresh': 'com.happyfresh.android'
 }
 
 def scrap_reviews(app_id, app_name):
@@ -15,12 +15,12 @@ def scrap_reviews(app_id, app_name):
         lang='id', # defaults to 'en'
         country='id', # defaults to 'us'
         sort=Sort.NEWEST, # defaults to Sort.NEWEST
-        count=100, # defaults to 100
+        count=500, # defaults to 100
         filter_score_with=None # defaults to None(means all score)
     )
     result, _ = reviews(
         app_id,
-        continuation_token=continuation_token # defaults to None(load from the beginning)
+        continuation_token=continuation_token # defaults to None(load from the beginning) 
     )
     return app_name, result
 
@@ -28,8 +28,12 @@ def main():
     for app_name, app_id in list_app.items():
         print(f'scraping {app_name}...')
         app_name, result = scrap_reviews(app_id, app_name)
+        print(result)
+        result_str = str(result)
+        utils.save_txt(result_str, f'data/{app_name}.txt')
+
         # utils.save_data(result, f'data/{app_name}.json')
-        utils.save_txt(str(result), f'data/{app_name}.txt')
+        # utils.save_txt(str(result), f'data/{app_name}.txt')
         print(f'{app_name} done...')
 
 
--- utils.py
+++ utils.py
@@ -1,17 +1,17 @@
 import json
 
 def save_data(data, filename):
-    with open(filename, 'w') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f)
 
 def load_data(filename):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
     
 def save_txt(data, filename):
-    with open(filename, 'w') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(data)
 
 def load_txt(filename):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return f.read()
